be82bbe3df33c480903f746c75cd38895b3166d2
Artluxdm <xolar1995@gmail.com>
2025-12-20 19:33:11 +0100

    debug: enable DevTools in production for debugging

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -165,6 +165,8 @@ async function createWindow() {
     win.webContents.openDevTools()
   } else {
     win.loadFile(indexHtml)
+    // Temporarily enable DevTools in production for debugging
+    win.webContents.openDevTools()
   }
 
   // Test actively push message to the Electron-Renderer
